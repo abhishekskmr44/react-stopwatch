493f7f4fb2a2d0ea198b1242be7ed65267a8992c
Abhishek Sukumar <abhishekskmr44@gmail.com>
2022-11-01 20:21:19 +0530

    seconds

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,53 +1,73 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 export const Timer = () => {
-
+////////////////////////////STATES\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 const [seconds, setSeconds] = useState(0)
 
 const [randomInput, setRandomInput] = useState("")
 
 const [minutes, setMinutes] = useState(0)
 
+const [hours, setHours] = useState(0)
 
 const renders = useRef(0)
 
 const timerId = useRef(0)
 
-
+///////////////////////////////USEFFECT\\\\\\\\\\\\\\\\\\\\\\\\\\\
 useEffect(()=>{
   if(seconds>59){
     setSeconds(0)
     setMinutes(minutes+1)
   }
+  if(minutes>59){
+    setMinutes(0)
+    setHours(hours+1)
+  }
 })
 
+
+////////////////////////////HANDLE CHANGE\\\\\\\\\\\\\\\\\\\\\\\\\\
 const handleChange = (e) => {
   setRandomInput(e.target.value)
   renders.current++
-  
 }
-
+/////////////////////Start Timer\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 const startTimer = () => {
-  
   timerId.current = setInterval(()=>{
     renders.current++
     setSeconds(prev=>prev+1)
-  },10)
-  
+  },0)
 }
 
+
+//////////////////////////STOP BUTTON\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ 
 const stopTimer = ()=>{
   clearInterval(timerId.current)
   timerId.current = 0;
 }
 
+
+
+///////////////////////////////////RESET BUTTON\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 const resetTimer = () => {
   stopTimer()
   if(seconds){
     renders.curent++;
     setSeconds(0);
   }
+  if(minutes){
+    renders.curent++;
+    setMinutes(0);
+  }
+  if(hours){
+    renders.curent++;
+    setHours(0);
+  }
 }
+
+
+
 
   return (
    
@@ -55,7 +75,7 @@ const resetTimer = () => {
     <input type="text" value={randomInput} placeholder="Random Input" onChange={handleChange}/>
     <p>Renders:{renders.current}</p>
     <h1>Timer</h1>
-    <p>{minutes}:{seconds}</p>
+    <p>{hours}:{minutes}:{seconds}</p>
     <br></br>
     <section>
     <button onClick={startTimer}>Start</button>
